de68e85269e9918b8309506f4c3d834f8878ac31
laowang <2479586109@qq.com>
2018-11-22 07:04:25 +0800

    练习

---
+++ 134.py
@@ -0,0 +1,16 @@
+#读取文件，每三个字符打印一次，间隔1S
+import time
+with open(r'C:\Users\zheng\Desktop\record.txt','r')as f:
+    t = f.read(3)
+    print(t)
+    while len(t):
+        t = f.read(3)
+        print(t, end='')
+        print()
+        #time.sleep(1)
+
+
+
+
+    # print()
+
